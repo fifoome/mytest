3662bd0f927d9c8a7d5b8cf1224bea4f3ad0cc4c
cazenave cyrille <thedmbp@cazenaves-MacBook-Pro.local>
2019-02-04 22:25:08 -0500

    e

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,26 @@
 from flask import Flask
+from flask import request
+import redis, toml
+
+with open("/etc/config/config.toml", "r") as f:
+    parsed_toml = toml.loads(f.read())
+
+r = redis.Redis(
+    host=parsed_toml["alias"]['redis'],
+    port=parsed_toml["alias"]['redisport'],
+    password='')
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hey, we have Flask in a Docker container! updated'
+    return 'Hey, we have Flask in a Docker container! \nthe value in redis is: ' + str(r.get("mykey"))
 
+@app.route('/setdata')
+def data():
+    # here we want to get the value of user (i.e. ?user=some-value)
+    r.set("mykey", request.args.get('data'))
+    return 'Set the key'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,28 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './config.json';
+var React = require('react/addons');
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+
+
+var List = React.createClass({
+  getInitialState: function() {
+    return {data: this.props.data};
+  },
+  render: function() {
+    var listItems = this.state.data.map(function(p) {
+      for (var key in p) {
+            if (p.hasOwnProperty(key)) {
+                return (
+                    <td>{p[key]}</td>
+                )
+            }
+        }
+    });
+    return <ul>{listItems}</ul>
   }
-}
+});
+var App = React.createClass({
+    render: function() {
+      return (
+        <div>
+          <List data={data}/>
+        </div>
+      );
+    }
+  });
+
+module.exports = App;
 
 export default App;
